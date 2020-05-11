44536dfbde1406cf9dad7a270fe80d73b2e35139
aleksandar <jocovic099@gmail.com>
2020-05-11 14:33:57 +0200

    Updates

---
+++ precache-manifest.e968b3b72b8672bf2aaf1ee1e4d75665.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc31c3b961b55a6cb9c278170eb5e0f3",
+    "url": "/Country-Info/index.html"
+  },
+  {
+    "revision": "ef3c5682cff0317c442d",
+    "url": "/Country-Info/static/css/main.a4625438.chunk.css"
+  },
+  {
+    "revision": "c5046d6465466ac4607d",
+    "url": "/Country-Info/static/js/2.e8afedb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Country-Info/static/js/2.e8afedb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3c5682cff0317c442d",
+    "url": "/Country-Info/static/js/main.c51b1d58.chunk.js"
+  },
+  {
+    "revision": "bb2fdbdde71cf7147a36",
+    "url": "/Country-Info/static/js/runtime-main.58dda6c3.js"
+  }
+]);
